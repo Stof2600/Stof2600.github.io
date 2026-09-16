a093403e8c9f400e426cbc7abe6de7942eb7c828
Stof2600 <143953206+Stof2600@users.noreply.github.com>
2026-09-16 20:10:34 +0200

    game description download

--- webpage/Script/GameBox.gd
+++ webpage/Script/GameBox.gd
@@ -1,29 +1,48 @@
 extends Control
 
 @onready var gameIcon: TextureRect = $GameIcon
+@onready var Title: Label = $GameName
+@onready var descr: Label = $Description
+
 @export var  gameName: String = "ADAURUM"
 
 var domainURL: String = "https://stof2600.github.io/"
 var iconURL: String
 var gameURL: String
+var descURL: String
 
 func _ready() -> void:
+	Title.text = gameName.to_upper()
+	
 	var baseURL: String = domainURL + "HTMLGames/" + gameName.to_upper() + "/"
 	iconURL = baseURL + "icon.png"
 	gameURL = baseURL + gameName
+	descURL = baseURL + "descr.txt"
 	
 	print(iconURL)
 	
 	DownloadIcon()
+	DownloadDesc()
 
 func DownloadIcon():
 	var webRequest = HTTPRequest.new()
+	webRequest.request_completed.connect(SetIcon)
+	webRequest.request_completed.connect(webRequest.queue_free.unbind(4))
 	add_child(webRequest)
-	webRequest.connect("request_completed", SetIcon)
 	
 	var ping = webRequest.request(iconURL)
 	if ping != OK:
 		print("ERROR GETTING GAME ICON")
+func DownloadDesc():
+	var webRequest = HTTPRequest.new()
+	webRequest.request_completed.connect(SetDesc)
+	webRequest.request_completed.connect(webRequest.queue_free.unbind(4))
+	add_child(webRequest)
+	
+	var ping = webRequest.request(descURL)
+	if ping != OK:
+		print("ERROR GETTING GAME DESCRIPTION")
+	pass
 
 @warning_ignore("unused_parameter")
 func SetIcon(result, code, head, body):
@@ -37,6 +56,10 @@ func SetIcon(result, code, head, body):
 		print("FAILED TO CREATE TEXTURE")
 	
 	gameIcon.texture = tex
+	pass
+@warning_ignore("unused_parameter")
+func SetDesc(result, code, head, body):
+	print(body)
 	pass
 
 func PlayGame():
